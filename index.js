3c4701b28c80380382efc7920e26e1ca6ec232a8
Ahmad Nurdin <ahmadnurdinalfarizi77@gmail.com>
2023-07-08 20:00:26 +0700

    Baru lagi nih

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
-// Add smooth scrolling to navigation links
-document.querySelectorAll("nav a").forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
-  });
-  console.log("hello world");
-});
+console.log("hello world");
